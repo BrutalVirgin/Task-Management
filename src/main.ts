85112bfeeba754e10f4ed33160ac6813d53c1641
Kirill Bondarenko <kirill.bondarenko@timecode.com>
2022-06-13 16:17:31 +0300

    fixes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
 import { taskRouter } from "./routes/task-router";
 import { userRouter } from "./routes/user-router";
 import mongoose from "mongoose";
-import path from "path";
+// import path from "path";
 
-dotenv.config({ path: path.resolve(__dirname, "./.env") });
+// dotenv.config({ path: path.resolve(__dirname, "./.env") });
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,7 +19,7 @@ async function start() {
     //mongodb+srv://Brutal:345124qe@tasks.7vencrt.mongodb.net/Tasks
     //process.env.DBURL as string
     await mongoose.connect(
-      "mongodb+srv://Brutal:345124qe@tasks.7vencrt.mongodb.net/Tasks"
+      "mongodb+srv://Brutal:345124qe@tasks.7vencrt.mongodb.net/Tasks/retryWrites=true&w=majority"
     );
     console.log("database connected");
     app.listen(process.env.PORT, () => console.log(`runnin`));
